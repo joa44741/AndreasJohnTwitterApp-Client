f7e4fb4be7670dc1c16fc25d679989843db9ee3c
joa44741 <john_andreas@gmx.net>
2018-01-02 17:23:15 +0100

    trying to add lightbox framework

--- src/services/formatter.js
+++ src/services/formatter.js
@@ -6,7 +6,7 @@ export default class Formatter {
     for (let index in tweets) {
       if (moment(tweets[index].creationDate).fromNow() !== 'Invalid date') {
         tweets[index].creationDate = moment(tweets[index].creationDate).format('DD.MM.YYYY HH:mm:ss');
-        tweets[index].formattedDate = moment(tweets[index].creationDate).fromNow();
+        tweets[index].formattedDate = moment(tweets[index].creationDate, 'DD.MM.YYYY HH:mm:ss').fromNow();
       }
     }
   }
